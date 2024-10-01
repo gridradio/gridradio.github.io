a0513279dd97ab61790e4c4c7bc64734a5da4699
gridradio <gridradio@oldershaw.org>
2024-10-01 01:11:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,18 @@
-// This is the "Offline copy of pages" service worker
-
-const CACHE = "pwabuilder-offline";
+const cacheName = 'grid-radio-cache-v1';
+const resourcesToCache = [
+    '/',
+    '/index.html',
+    '/bootstrap.min.css',
+    '/leaflet.css',
+    '/Control.FullScreen.css',
+    '/easy-button.css',
+    '/leaflet.js',
+    '/L.Maidenhead.js',
+    '/Control.FullScreen.js',
+    '/easy-button.js',
+    '/proj4.js',
+    '/manifest.json'
+];
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -13,6 +25,22 @@ self.addEventListener("message", (event) => {
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+    cacheName: cacheName
   })
 );
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(resourcesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
